bf031da89d699085f1cbf152ec9077f74d1e4b38
mario-peters <42603363+mario-peters@users.noreply.github.com>
2018-11-20 10:22:52 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -2,3 +2,7 @@
 #
 # Author: GizMoCuz
 #
+"""
+<plugin key="Afvalkalender" name="Afvalkalender" author="mario-peters" version="1.0.0" wikilin="https://github.com/mario-peters/Afvalkalender" externallink="https://github.com/mario-peters/Afvalkalender">
+</plugin>
+"""
